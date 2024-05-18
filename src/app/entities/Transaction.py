ad143b270972f4933ce8402a6853885a7adabfcf
lseixas <lseixas.iorio@gmail.com>
2024-05-18 18:11:31 -0300

    ajustes sintaxe

--- src/app/entities/Transaction.py
+++ src/app/entities/Transaction.py
@@ -2,13 +2,15 @@ from typing import Tuple
 from ..errors.entity_errors import ParamNotValidated
 from ..enums.transaction_type_enum import TransactionTypeEnum
 
+
 class Transaction:
     type: TransactionTypeEnum
     value: float
     current_balance: float
     timestamp: float
 
-    def __init__(self, type: TransactionTypeEnum=None, value: float=None, current_balance: float=None, timestamp: float=None):
+    def __init__(self, type: TransactionTypeEnum = None, value: float = None, current_balance: float = None,
+                 timestamp: float = None):
 
         validation_type = self.validate_type(type)
         if validation_type[0] is False:
@@ -30,45 +32,43 @@ class Transaction:
             raise ParamNotValidated("tempo", validation_timestamp[1])
         self.timestamp = timestamp
 
-    #validações de campos 
     @staticmethod
     def validate_type(transaction_type: TransactionTypeEnum) -> Tuple[bool, str]:
         if type is None:
-            return (False, "Tipo é obrigatório")
-        if type(transaction_type) != TransactionTypeEnum:
-            return (False, "Tipo inválido")
-        return (True, "")
-    
+            return False, "Tipo é obrigatório"
+        if type(transaction_type) is not TransactionTypeEnum:
+            return False, "Tipo inválido"
+        return True, ""
+
     @staticmethod
     def validate_value(value: float) -> Tuple[bool, str]:
         if value is None:
-            return (False, "Valor é obrigatório")
-        if type(value) != float:
-            return (False, "Valor deve ser um float")
+            return False, "Valor é obrigatório"
+        if type(value) is not float:
+            return False, "Valor deve ser um float"
         if value < 0:
-            return (False, "Uma transação deve conter um valor positivo")
-        return (True, "") 
-    
+            return False, "Uma transação deve conter um valor positivo"
+        return True, ""
 
     @staticmethod
     def validate_current_balance(current_balance: float) -> Tuple[bool, str]:
         if current_balance is None:
-            return (False, "Saldo é obrigatório")
-        if type(current_balance) != float:
-            return (False, "Saldo deve ser um float, Ex: 1000.0")
+            return False, "Saldo é obrigatório"
+        if type(current_balance) is not float:
+            return False, "Saldo deve ser um float, Ex: 1000.0"
         if current_balance < 0:
-            return (False, "Saldo deve ser um número positivo")
-        return (True, "")
-    
-    @staticmethod 
+            return False, "Saldo deve ser um número positivo"
+        return True, ""
+
+    @staticmethod
     def validate_timestamp(timestamp: float) -> Tuple[bool, str]:
         if timestamp is None:
-            return (False, "Tempo é obrigatório")
-        if type(timestamp) != float:
-            return (False, "Tempo deve ser um float")
+            return False, "Tempo é obrigatório"
+        if type(timestamp) is not float:
+            return False, "Tempo deve ser um float"
         if timestamp < 0:
-            return (False, "Tempo deve ser um número positivo")
-        return (True, "")
+            return False, "Tempo deve ser um número positivo"
+        return True, ""
 
     def to_dict(self):
         return {
@@ -77,9 +77,15 @@ class Transaction:
             "current_balance": self.current_balance,
             "timestamp": self.timestamp,
         }
-    
-    def __eq__(self,other):
-        return self.type == other.type and self.value == other.value and self.current_balance == other.current_balance and self.timestamp == other.timestamp
+
+    def __eq__(self, other):
+        return (self.type == other.type and
+                self.value == other.value and
+                self.current_balance == other.current_balance and
+                self.timestamp == other.timestamp)
 
     def __repr__(self):
-        return f"Transaction(type={self.type}, value={self.value}, current_balance={self.current_balance}, timestamp={self.timestamp})"
+        return (f"Transaction(type={self.type}, "
+                f"value={self.value}, "
+                f"current_balance={self.current_balance}, "
+                f"timestamp={self.timestamp})")
--- src/app/entities/user.py
+++ src/app/entities/user.py
@@ -1,8 +1,6 @@
 from typing import Tuple
 from ..errors.entity_errors import ParamNotValidated
-from ..enums.item_type_enum import ItemTypeEnum
 import re #importado para a verificação de senha
-
 
 class User:
     name: str
@@ -10,13 +8,14 @@ class User:
     conta: str
     current_balance: float
     admin_permission: bool = False
-    
-    def __init__(self, name: str=None, agencia: str = None, conta: str=None, current_balance:float=None, admin_permission: bool=None):
+
+    def __init__(self, name: str = None, agencia: str = None, conta: str = None, current_balance: float = None,
+                 admin_permission: bool = None):
         validation_name = self.validate_name(name)
         if validation_name[0] is False:
             raise ParamNotValidated("nome", validation_name[1])
         self.name = name
-        
+
         validation_agencia = self.validate_agencia(agencia)
         if validation_agencia[0] is False:
             raise ParamNotValidated("agencia", validation_agencia[1])
@@ -27,11 +26,11 @@ class User:
             raise ParamNotValidated("conta", validation_conta[1])
         self.conta = conta
 
-        valodation_current_balance = self.validate_current_balance(current_balance)
-        if valodation_current_balance[0] is False:
-            raise ParamNotValidated("Saldo", valodation_current_balance[1])
+        validation_current_balance = self.validate_current_balance(current_balance)
+        if validation_current_balance[0] is False:
+            raise ParamNotValidated("Saldo", validation_current_balance[1])
         self.current_balance = current_balance
-        
+
         validation_admin_permission = self.validate_admin_permission(admin_permission)
         if validation_admin_permission[0] is False:
             raise ParamNotValidated("admin_permission", validation_admin_permission[1])
@@ -40,60 +39,60 @@ class User:
     @staticmethod
     def validate_name(name: str) -> Tuple[bool, str]:
         if name is None:
-            return (False, "Nome é necessário")
-        if type(name) != str:
-            return (False, "Nome precisa ser uma string")
+            return False, "Nome é necessário"
+        if type(name) is not str:
+            return False, "Nome precisa ser uma string"
         if len(name) < 3:
-            return (False, "Nome precisa ter pelo menos 3 caracteres")
-        return (True, "")
-        
+            return False, "Nome precisa ter pelo menos 3 caracteres"
+        return True, ""
+
     @staticmethod
     def validate_agencia(agencia: str) -> Tuple[bool, str]:
         if agencia is None:
-            return (False, "Uma agência é necesária")
-        if type(agencia) != str:
-            return (False, "A agência precisa ser uma string")
+            return False, "Uma agência é necesária"
+        if type(agencia) is not str:
+            return False, "A agência precisa ser uma string"
         if len(agencia) != 4:
-            return (False, "A agência precisa conter apenas 4 digitos")
+            return False, "A agência precisa conter apenas 4 digitos"
 
         try:
             int(agencia)
         except ValueError:
-            return (False, "A agencia precisa conter 4 digitos no formato (xxxx), com x indo de 0 a 9")
+            return False, "A agencia precisa conter 4 digitos no formato (xxxx), com x indo de 0 a 9"
 
-        return (True, "")
-    
+        return True, ""
+
     @staticmethod
     def validate_conta(conta: str) -> Tuple[bool, str]:
         if conta is None:
-            return (False, "É necessária uma conta")
-        if type(conta) != str:
-            return (False, "A conta precisa ser uma string")
+            return False, "É necessária uma conta"
+        if type(conta) is not str:
+            return False, "A conta precisa ser uma string"
         else:
             if conta.find("-") != 5:
-                return (False, "Formato inválido a")
+                return False, "Formato inválido, conta precisa de - no 5º digito"
             if len(conta) != 7:
-                return (False, "Formato inválido b")
-        return (True, "")
-    
+                return False, "Formato inválido, conta precisa ter 7 digitos"
+        return True, ""
+
     @staticmethod
     def validate_current_balance(current_balance: float) -> Tuple[bool, str]:
         if current_balance is None:
-            return (False, "Saldo é necessário")
-        if type(current_balance) != float and current_balance != 0:
-            return (False, "Saldo precisa ser um float")
+            return False, "Saldo é necessário"
+        if type(current_balance) is not float and current_balance != 0:
+            return False, "Saldo precisa ser um float"
         if current_balance < 0:
-            return (False, "Saldo precisa ser um número positivo")
-        return (True, "")
-    
+            return False, "Saldo precisa ser um número positivo"
+        return True, ""
+
     @staticmethod
     def validate_admin_permission(admin_permission: bool) -> Tuple[bool, str]:
         if admin_permission is None:
-            return (False, "Permissão de administrador é necessária")
-        if type(admin_permission) != bool:
-            return (False, "Permissão de administrador precisa ser um booleano")
-        return (True, "")
-            
+            return False, "Permissão de administrador é necessária"
+        if type(admin_permission) is not bool:
+            return False, "Permissão de administrador precisa ser um booleano"
+        return True, ""
+
     def to_dict(self):
         return {
             "name": self.name,
@@ -102,9 +101,13 @@ class User:
             "current_balance": self.current_balance,
             "admin_permission": self.admin_permission
         }
-    
-    def __eq__(self,other):
+
+    def __eq__(self, other):
         return self.name == other.name and self.agencia == other.agencia and self.conta == other.conta
-    
+
     def __repr__(self):
-        return f"Item(name={self.name}, agencia={self.agencia}, conta={self.conta})"
+        return (f"User(name={self.name}, "
+                f"agencia={self.agencia}, "
+                f"conta={self.conta}), "
+                f"current_balance={self.current_balance}, "
+                f"admin_permission={self.admin_permission})")
